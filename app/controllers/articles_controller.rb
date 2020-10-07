c1d192c36680039cd613c1bb7002a265ecd9367b
sburakbahceci <safaburakbahceci29@gmail.com>
2020-10-07 11:29:26 +0300

    Add association between user and article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,10 +7,10 @@ class ArticlesController < ApplicationController
         
     end
     def new
-        @article = Article.new
+        @article = current_user.articles.build
     end
     def create
-        @article = Article.new(article_params)
+        @article = current_user.articles.build(article_params)
         if @article.save
             redirect_to @article
         else
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
     end
     private
     def find_article
-        @article = Article.find(params[:id])
+        @article = Article.find(params[:id]) 
     end
     def article_params
         params.require(:article).permit(:title, :content)
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_080218) do
+ActiveRecord::Schema.define(version: 2020_10_07_082121) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
